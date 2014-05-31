9e70cfc21f3d6f82ce586cb291299f503113a512
Mike Hockerman <mike@hockerman.com>
2014-05-31 13:32:15 -0400

    Added code to get linear Z acceleration

--- android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/service/SensorService.java
+++ android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/service/SensorService.java
@@ -2,24 +2,45 @@ package com.indyhack.civicpotholes.service;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 /**
  * Created by mike on 5/31/14.
  */
-public class SensorService {
+public class SensorService implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor sensor;
 
+    private double currentX, currentY, currentZ;
+    private int accuracy;
+
     public SensorService(Context c) {
         sensorManager = (SensorManager) c.getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorManager.registerListener(this, sensor, 0);
     }
 
-    public void getLinearAcceleration() {
+    public double getLinearZAcceleration() {
+        return currentZ;
+    }
 
+    public void close() {
+        sensorManager.unregisterListener(this);
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        currentX = event.values[0];
+        currentY = event.values[1];
+        currentZ = event.values[2];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        this.accuracy = accuracy;
+    }
 }
