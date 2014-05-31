2463264833b3ac6168059f45dc84b0f90eb0aefb
Mike Hockerman <mike@hockerman.com>
2014-05-31 13:09:02 -0400

    Added debug keystore

--- android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/service/SensorService.java
+++ android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/service/SensorService.java
@@ -1,7 +1,25 @@
 package com.indyhack.civicpotholes.service;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
 /**
  * Created by mike on 5/31/14.
  */
 public class SensorService {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    public SensorService(Context c) {
+        sensorManager = (SensorManager) c.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    }
+
+    public void getLinearAcceleration() {
+
+    }
+
+
 }
