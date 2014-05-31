b1ddaf797fa1c65f90d27c9408f3eba9b5fe648c
Ben Wencke <benwencke@gmail.com>
2014-05-31 15:48:17 -0400

    Added marker for real

--- android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/GetLatLngTask.java
+++ android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/GetLatLngTask.java
@@ -1,9 +1,6 @@
 package com.indyhack.civicpotholes;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
@@ -15,24 +12,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Locale;
 
