60bb622ab1e1a7ca9beccaab5277f7745ab4f3ec
Ben Wencke <benwencke@gmail.com>
2014-05-31 14:20:49 -0400

    Added API call

--- android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/PopulateMapTask.java
+++ android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/PopulateMapTask.java
@@ -4,10 +4,25 @@ import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Locale;
 
@@ -25,6 +40,28 @@ public class PopulateMapTask extends AsyncTask<Void, Integer, List<LatLng>> {
 
     @Override
     protected List<LatLng> doInBackground(Void... params) {
+        HttpResponse response = null;
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI("http://communities.socrata.com/resource/8f5a-5xkb.json?$select=incident_address&status=Open"));
+            response = client.execute(request);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String result = null;
+        try {
+            result = convertStreamToString(response.getEntity().getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("data", result);
         return null;
     }
 
@@ -50,6 +87,26 @@ public class PopulateMapTask extends AsyncTask<Void, Integer, List<LatLng>> {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private String convertStreamToString(InputStream inputStream) throws IOException {
+        if (inputStream != null) {
+            Writer writer = new StringWriter();
+
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"),1024);
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                inputStream.close();
+            }
+            return writer.toString();
+        } else {
+            return "";
         }
     }
 }
