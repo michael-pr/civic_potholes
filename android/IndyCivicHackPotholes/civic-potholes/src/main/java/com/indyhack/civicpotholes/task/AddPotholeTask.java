d84ee5d953f860bc2775142cce650919a4acec9b
Mike Hockerman <mike@hockerman.com>
2014-05-31 18:06:39 -0400

    Added task to post new pothole.

---
+++ android/IndyCivicHackPotholes/civic-potholes/src/main/java/com/indyhack/civicpotholes/task/AddPotholeTask.java
@@ -0,0 +1,46 @@
+package com.indyhack.civicpotholes.task;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+/**
+ * Created by mike on 5/31/14.
+ */
+public class AddPotholeTask extends AsyncTask<Void, Void, Void> {
+
+    private double lat;
+    private double lng;
+
+    public AddPotholeTask(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        HttpClient c = new DefaultHttpClient();
+        HttpPost post = new HttpPost();
+
+        String contents = "{latitude:"+lat+",longitude:"+lng+"}";
+        HttpEntity entity = new ByteArrayEntity(contents.getBytes());
+        post.setEntity(entity);
+
+        try {
+            c.execute(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return null;
+    }
+
+}
